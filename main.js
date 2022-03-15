2bd47c378676d5d6f3cae4803a5ea1b8b773f033
Hugo Shinde <hugo.shinde@k2m.com.br>
2022-03-15 08:14:29 -0300

    Add repetition functions and dynamic values

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-document.querySelector('.tecla_pom')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
+
+}
